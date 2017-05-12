ebb6e47a552618f0b38e4bb9863c1c6bf5215c14
afincato <andre.not.me@gmail.com>
2017-05-13 00:46:40 +0200

    set loop to iterate over all `.todo.txt` files except for `today.todo.txt`

--- today.py
+++ today.py
@@ -1,27 +1,32 @@
-import re
+import os
+import glob
 import datetime
 
-source_path = 'arachne.todo.txt'
-todo_file = open(source_path, 'r')
-todos = todo_file.readlines()
+# source_path = 'arachne.todo.txt'
+# todo_file = open(source_path, 'r')
+# todos = todo_file.readlines()
 
 dest_path = 'today.todo.txt'
 today_todo = open(dest_path, 'w')
 
-title = '# Today \n\n'
+title = '# Today \n'
 today_todo.write(title)
 
 date_today = datetime.date.today()
 date_today = date_today.strftime('%Y-%m-%d')
 
-project_title = '#' + todos[0] + ' \n'
-today_todo.write(project_title)
+for file in set(glob.glob('*todo.txt')) - set(glob.glob('today.todo.txt')):
+  print(file)
+  with open(file) as fp:
+    project = fp.readlines()
+    project_title = '\n#' + project[0] + ' \n'
+    today_todo.write(project_title)
 
-for item in todos:
-  if '@due' in item:
-    if date_today in item:
-      today_todo.write(item)
+    for item in project:
+      if '@due' in item:
+        if date_today in item:
+          today_todo.write(item)
 
 
-todo_file.close()
+fp.close()
 today_todo.close()
