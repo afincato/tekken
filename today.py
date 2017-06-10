e38b80da6fcf7b70e402b865765b8eb4ed96fcf7
afincato <andre.not.me@gmail.com>
2017-06-10 18:07:47 +0200

    clean up and add parsing for grabbing undone, due-today items

--- today.py
+++ today.py
@@ -5,25 +5,28 @@ import datetime
 dest_path = 'today.todo.txt'
 today_todo = open(dest_path, 'w')
 
-title = '# Today \n'
-today_todo.write(title)
-
 date_today = datetime.date.today()
 date_today = date_today.strftime('%d-%m-%Y')
+date_title = date_today + '\n----------\n'
+
+title = '# Today\n\n'
+today_todo.write(title)
+print title
+today_todo.write(date_title)
+print date_title
 
 for file in set(glob.glob('*todo.txt')) - set(glob.glob('today.todo.txt')):
-  print(file)
   with open(file) as fp:
     project = fp.readlines()
     project_title = '\n#' + project[0] + ' \n'
     today_todo.write(project_title)
+    print project_title
 
     for item in project:
-      if '@due' in item:
-        if date_today in item:
+      if date_today in item:
+        if '- [ ]' in item:
           today_todo.write(item)
-          print(item)
-
+          print item
 
 fp.close()
 today_todo.close()
