c3f6c042f9b8dedbb391f93dc276af8932fd6fcb
afincato <andre.not.me@gmail.com>
2017-07-17 00:01:19 +0200

    replace for loop to list comprehension

--- today.py
+++ today.py
@@ -1,32 +1,57 @@
 import os
 import glob
 import datetime
+import re
 
-dest_path = 'today.todo.txt'
-today_todo = open(dest_path, 'w')
+# --- read from each project.todo
+# --- && write a comprehensive list to today.todo
 
 date_today = datetime.date.today()
 date_today = date_today.strftime('%d-%m-%Y')
 date_title = date_today + '\n----------\n'
 
-title = '# Today\n\n'
-today_todo.write(title)
-print title
-today_todo.write(date_title)
-print date_title
-
 for file in set(glob.glob('*todo.txt')) - set(glob.glob('today.todo.txt')):
+  
   with open(file) as fp:
     project = fp.readlines()
-    project_title = '\n#' + project[0] + ' \n'
-    today_todo.write(project_title)
-    print project_title
+    # print(project)
 
-    for item in project:
-      if date_today in item:
-        if '- [ ]' in item:
-          today_todo.write(item)
-          print item
+    print('\n' + project[0])
 
-fp.close()
-today_todo.close()
+    tasks_due = [item for item in project
+                 if date_today in item
+                 if '- [ ]' in item]
+    
+    tasks_done = [item for item in project
+                  if date_today in item
+                  if '- [x]' in item]
+
+    # todo: add `~~item~~` right after `- [x]` &&
+    #       before any `@due(dd-mm-yyyy)`
+
+    # merge two lists and put done tasks at the end
+    items = tasks_due + tasks_done
+    print('\n'.join(str(item) for item in items))
+
+
+# --- write all projects to today.todo
+# with open('today.todo.txt', 'w') as tf:
+
+#   title = '# Today\n\n'
+#   tf.write(title)
+#   print(title)
+  
+#   tf.write(date_title)
+#   print(date_title)
+
+
+#   # project title
+#   project_title = '\n#' + project[0] + ' \n'
+#   tf.write(project_title)
+
+#   tf.write('\n'.join(str(item) for item in items))
+#   for item in items:
+#     print(item)
+
+# ------
+# if __name__ = "__main__":
